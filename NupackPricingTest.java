d552799f39a2d3fe4ab3623eb7b87eaf2f4bf4fe
xWang94 <andrewx.wang@mail.utoronto.ca>
2014-03-25 17:29:47 -0400

    first commit: Created basic structure of JUnit testing for NupackPricing.java

---
+++ NupackPricingTest.java
@@ -0,0 +1,37 @@
+/*
+ * JUnit testing for NupackPricing.java
+ */
+package nupackpricing;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author Xiao Wang
+ */
+public class NupackPricingTest {
+    
+    NupackPricing nupack = new NupackPricing();
+    
+    public NupackPricingTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    /**
+     * Test of computePrice method, of class NupackPricing.
+     */
+    @Test
+    public void testComputePrice() {
+        // Test entire compute method in Nupackpricing.java
+    }
+ }
