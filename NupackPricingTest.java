c575bbbe64276ef495f76295f8ebe4e1c9d7f76f
xWang94 <andrewx.wang@mail.utoronto.ca>
2014-03-25 18:02:26 -0400

    Update NupackPricingTest.java
    Added individual test cases for flat markup, number of worker's markup, and electronics markup. The other 3 types of materials (pharmaceutical, food, and others) were already tested with the examples provided in PDF.

--- NupackPricingTest.java
+++ NupackPricingTest.java
@@ -28,38 +28,72 @@ public class NupackPricingTest {
     }
 
     /**
+    * Test of computePrice method, of class NupackPricing.
+    * Testing correctness for flat markup (no workers or materials involved).
+    */
+    @Test
+    public void testComputeFlatMarkup() {
+         System.out.println("Testing correctness for flat markup.");
+         double result = nupack.computePrice(1000, 0, "furniture");
+         assertEquals(result, 1050.00, 0.005);
+    }
+    
+    /**
+    * Test of computePrice method, of class NupackPricing.
+    * Testing correctness for worker's markup.
+    */
+    @Test
+    public void testCompute() {
+         System.out.println("Testing correctness for flat markup.");
+         double result = nupack.computePrice(1000, 10, "furniture");
+         assertEquals(result, 1176.00, 0.005);
+    }
+   
+    /**
      * Test of computePrice method, of class NupackPricing.
-     * Testing input example 1 on PDF
+     * Testing input example 1 on PDF, food markup is required.
      */
     @Test
-    public void testComputeExample1() {
-        System.out.println("Compute Input 1 example.");
+    public void testComputeFood() {
+        System.out.println("Testing correctness for food materials.");
         double result = nupack.computePrice(1299.99, 3, "food");
         
         // When comparing results, delta allows 0.005 difference because
         // it is round to 2 decimal places.
         assertEquals(result, 1591.58, 0.005);  
-   }    
-   
+    }    
+  
     /**
      * Test of computePrice method, of class NupackPricing.
-     * Testing input example 2 on PDF
+     * Testing input example 2 on PDF, pharmaceuticals markup is required.
      */
     @Test
-    public void testComputeExample2() {
-        System.out.println("Compute Input 2 example.");
+    public void testComputePharmaceuticals() {
+        System.out.println("Testing correctness for pharmaceutical materials.");
         double result = nupack.computePrice(5432.00, 1, "drugs");
         assertEquals(result, 6199.81, 0.005);
-   }
+    }
     
-        /**
+    /**
      * Test of computePrice method, of class NupackPricing.
-     * Testing input example 3 on PDF
+     * Testing input example 3 on PDF, other materials do not require markup.
      */
     @Test
-    public void testComputeExample3() {
-        System.out.println("Compute Input 3 example.");
+    public void testComputeOtherMaterials() {
+        System.out.println("Testing correctness for other materials.");
         double result = nupack.computePrice(12456.95, 4, "books");
         assertEquals(result, 13707.63, 0.005);
    }
+   
+  /**
+   * Test of computePrice method, of class NupackPricing.
+   * Testing correctness for electronic material markup.
+   */
+   @Test
+    public void testComputeElectronics() {
+        System.out.println("Testing correctness for electronic material markup.");
+        double result = nupack.computePrice(1000, 0, "electronics");
+        assertEquals(result, 1071.00, 0.005);
+   }   
+    
  }
