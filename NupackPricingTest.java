a4a6aaefc88913fbc27a22e1c424ca24655fb0c5
xWang94 <andrewx.wang@mail.utoronto.ca>
2014-03-25 17:40:00 -0400

    Update NupackPricingTest.java
    Added test case for the 3 examples provided in PDF.

--- NupackPricingTest.java
+++ NupackPricingTest.java
@@ -29,9 +29,37 @@ public class NupackPricingTest {
 
     /**
      * Test of computePrice method, of class NupackPricing.
+     * Testing input example 1 on PDF
      */
     @Test
-    public void testComputePrice() {
-        // Test entire compute method in Nupackpricing.java
-    }
+    public void testComputeExample1() {
+        System.out.println("Compute Input 1 example.");
+        double result = nupack.computePrice(1299.99, 3, "food");
+        
+        // When comparing results, delta allows 0.005 difference because
+        // it is round to 2 decimal places.
+        assertEquals(result, 1591.58, 0.005);  
+   }    
+   
+    /**
+     * Test of computePrice method, of class NupackPricing.
+     * Testing input example 2 on PDF
+     */
+    @Test
+    public void testComputeExample2() {
+        System.out.println("Compute Input 2 example.");
+        double result = nupack.computePrice(5432.00, 1, "drugs");
+        assertEquals(result, 6199.81, 0.005);
+   }
+    
+        /**
+     * Test of computePrice method, of class NupackPricing.
+     * Testing input example 3 on PDF
+     */
+    @Test
+    public void testComputeExample3() {
+        System.out.println("Compute Input 3 example.");
+        double result = nupack.computePrice(12456.95, 4, "books");
+        assertEquals(result, 13707.63, 0.005);
+   }
  }
