1a985388b7f60608325af5dde3614d5e5ea2f201
xWang94 <andrewx.wang@mail.utoronto.ca>
2014-03-25 23:35:38 -0400

    Update NupackPricingTest.java
    Deleted unnecessary commands.

--- NupackPricingTest.java
+++ NupackPricingTest.java
@@ -3,10 +3,8 @@
  */
 package nupackpricing;
 
-import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import org.junit.BeforeClass;
 
 /**
  *
@@ -16,24 +14,12 @@ public class NupackPricingTest {
     
     NupackPricing nupack = new NupackPricing();
     
-    public NupackPricingTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
     /**
     * Test of computePrice method, of class NupackPricing.
     * Testing correctness for flat markup (no workers or materials involved).
     */
     @Test
     public void testComputeFlatMarkup() {
-         System.out.println("Testing correctness for flat markup.");
          double result = nupack.computePrice(1000, 0, "furniture");
          assertEquals(result, 1050.00, 0.005);
     }
@@ -44,7 +30,6 @@ public class NupackPricingTest {
     */
     @Test
     public void testCompute() {
-         System.out.println("Testing correctness for flat markup.");
          double result = nupack.computePrice(1000, 10, "furniture");
          assertEquals(result, 1176.00, 0.005);
     }
@@ -55,7 +40,6 @@ public class NupackPricingTest {
      */
     @Test
     public void testComputeFood() {
-        System.out.println("Testing correctness for food materials.");
         double result = nupack.computePrice(1299.99, 3, "food");
         
         // When comparing results, delta allows 0.005 difference because
@@ -69,7 +53,6 @@ public class NupackPricingTest {
      */
     @Test
     public void testComputePharmaceuticals() {
-        System.out.println("Testing correctness for pharmaceutical materials.");
         double result = nupack.computePrice(5432.00, 1, "drugs");
         assertEquals(result, 6199.81, 0.005);
     }
@@ -80,7 +63,6 @@ public class NupackPricingTest {
      */
     @Test
     public void testComputeOtherMaterials() {
-        System.out.println("Testing correctness for other materials.");
         double result = nupack.computePrice(12456.95, 4, "books");
         assertEquals(result, 13707.63, 0.005);
    }
@@ -91,9 +73,7 @@ public class NupackPricingTest {
    */
    @Test
     public void testComputeElectronics() {
-        System.out.println("Testing correctness for electronic material markup.");
         double result = nupack.computePrice(1000, 0, "electronics");
         assertEquals(result, 1071.00, 0.005);
    }   
-    
- }
+}
