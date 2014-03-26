59de5e0f4648aa7bdd1b56cfd4816e7222a67f07
xWang94 <andrewx.wang@mail.utoronto.ca>
2014-03-25 23:16:18 -0400

    Added block comment and empty method for the NupackPricing class.

---
+++ NupackPricing.java
@@ -0,0 +1,33 @@
+/*---------------------------------
+ * Class NupackPricing
+ * 
+ * Author: Xiao Wang
+ * 
+ * Purpose: Compute the total price (totalPrice) of the service based on the 
+ *          prodcut's base price (basePrice), number of workers 
+ *          (numberOfWorkers), and the types of materials (material) involved.
+ * 
+ * Pre-condition: The basePrice and numberOfWorkers must be real numbers
+ *                greater than 0. 
+ * 
+ * Post-condition: All parameter variables are unchanged. The totalPrice has 
+ *                 been identified. Return the totalPrice
+ * 
+ * Parameters:
+ *              basePrice -- The base cost of the service.
+ *              numberOfWorkers -- Amount of workers involved in the job.
+ *              material -- The type of material involved in the job.
+ * 
+ * Returns: The total price (totalPrice) required from the service.
+ */
+ 
+package nupackpricing;
+
+
+public class NupackPricing {
+
+    public double computePrice(double basePrice, int numberOfPeople, String material) {
+       
+    }
+
+}
